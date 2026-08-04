8f24488399d166ec19a893f02bd3dce9167802eb
김지윤 <b01090443093@gmail.com>
2026-08-04 14:17:00 +0900

    [Chore] 초기 설정 및 의존성 추가

---
+++ src/test/java/feat/apex_BE/ApexBeApplicationTests.java
@@ -0,0 +1,13 @@
+package feat.apex_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
