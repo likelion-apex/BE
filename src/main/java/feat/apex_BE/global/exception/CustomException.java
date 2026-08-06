69d78ce4d57d3f56fa374f835281103545099650
원지현 <gtgsky2022@gmail.com>
2026-08-06 15:53:40 +0900

    [Feat] 커스텀 예외 클래스 CustomException 구현

---
+++ src/main/java/feat/apex_BE/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package feat.apex_BE.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
